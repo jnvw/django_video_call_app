eaa0d15b60e7a213249fa44bb3d0ad4c5106750c
Sumedh Bhagat <89976965+jnvw@users.noreply.github.com>
2024-02-13 00:08:26 +0530

    Update settings.py

--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6fdgh7tki&^y_-cl4%tpyl-$kh9($vos&a5q&uq%!az2gs__bz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
